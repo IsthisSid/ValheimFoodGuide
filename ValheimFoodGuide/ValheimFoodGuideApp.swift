d685b46c024805dc240e817295b5759cbe9c1880
Sidany Walker <sidanyk@gmail.com>
2022-05-25 11:25:54 -0400

    Initial Commit

---
+++ ValheimFoodGuide/ValheimFoodGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValheimFoodGuideApp.swift
+//  ValheimFoodGuide
+//
+//  Created by Sidany Walker on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct ValheimFoodGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
