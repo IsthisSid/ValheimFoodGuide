ebb83c8e7d3b8f37d37019f8216170137fe2ea9c
Sidany Walker <sidanyk@gmail.com>
2022-07-31 08:35:16 -0400

    Add more food items with stats

--- ValheimFoodGuide/Data/FoodData.swift
+++ ValheimFoodGuide/Data/FoodData.swift
@@ -32,6 +32,30 @@ let foodData: [Food] = [
         image: "Honey",
         gradientColors: [Color("ColorHoneyLight"), Color("ColorHoneyDark")],
         description: "When you break a beehive, you receive a queen bee which can be used to craft your own beehives at your base to create a honey farm.",
-        nutrition: ["8", "35", "900s", "1hp/tick"])
+        nutrition: ["8", "35", "900s", "1hp/tick"]),
+    
+    Food(
+        title: "Onion",
+        headline: "Onion is a farmable food. They are obtained in the mountain biome as seeds, normally located in chests in locations such as ruined shacks.",
+        image: "Onion",
+        gradientColors: [Color("ColorHoneyLight"), Color("ColorHoneyDark")],
+        description: "Onions are grown by planting Onion seeds with the Cultivator. It takes between 6000s and 7000s (between 100m and 116m36s, or about 3.33-3.88 in-game days) for them to fully grow.",
+        nutrition: ["13", "40", "900s", "1hp/tick"]),
+    
+    Food(
+        title: "Mushroom",
+        headline: "Mushrooms can be found on the ground in the Meadows, Black Forest and Swamp. ",
+        image: "Mushroom",
+        gradientColors: [Color("ColorRaspberryLight"), Color("ColorRaspberryDark")],
+        description: "Mushrooms can be used to craft recipes such as carrot soup and serpent stew. They can also be fed to hungry boars to either begin or continue the taming process, or to allow reproduction.",
+        nutrition: ["15", "15", "900s", "1hp/tick"]),
+    
+    Food(
+        title: "Yellow Mushroom",
+        headline: "Yellow mushrooms are commonly found in Burial Chambers or Troll Caves in the Black Forest and Sunken Crypts in the Swamp.",
+        image: "GoldMushroom",
+        gradientColors: [Color("ColorHoneyLight"), Color("ColorHoneyDark")],
+        description: "They can be eaten raw, or used at a Cauldron to craft Mead base: Minor stamina and Mead base: Medium stamina.",
+        nutrition: ["10", "30", "600s", "1hp/tick"])
     
 ]
